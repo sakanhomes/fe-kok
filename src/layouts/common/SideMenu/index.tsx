da4ea80a1b57f5482d287d8e95065d816ed69f38
Jeremy <callmejeremygartner@gmail.com>
2023-03-02 01:24:43 +0200

    Sidebar modifications (open on hover event)

--- src/layouts/common/SideMenu/index.tsx
+++ src/layouts/common/SideMenu/index.tsx
@@ -1,12 +1,10 @@
 import { LanguageIcon } from '@/components/icons/LanguageIcon'
-import { SideExpandIcon } from '@/components/icons/SideExpandIcon'
 import { Text } from '@/components/Text'
 import { ROUTES } from '@/constants/routes'
 import Box from '@/styles/Box'
 import useTranslation from 'next-translate/useTranslation'
 import Link from 'next/link'
-import React, { FC, Fragment } from 'react'
-import { BaseButton } from '@/components/buttons/BaseButton'
+import React, { FC } from 'react'
 import { SidebarList } from '../SidebarList'
 import { followingMockData, menuData } from './data'
 import * as S from './styled'
@@ -17,7 +15,11 @@ export const SideMenu: FC<{ open: boolean; toggleMenu: () => void }> = ({
 }) => {
   const { t } = useTranslation('layout')
   return (
-    <S.Wrapper open={open} onClick={!open ? toggleMenu : undefined}>
+    <S.Wrapper
+      open={open}
+      onMouseEnter={!open ? toggleMenu : undefined}
+      onMouseLeave={open ? toggleMenu : undefined}
+    >
       <Link href={ROUTES.HOME} passHref>
         <S.StyledLogo open={open} />
       </Link>
@@ -29,17 +31,12 @@ export const SideMenu: FC<{ open: boolean; toggleMenu: () => void }> = ({
         isUsersList
       />
       {open && (
-        <>
-          <Box display="flex" mb="22px" gridGap={[14]} alignItems="center">
-            <LanguageIcon />
-            <Text tag="span" variant="p3">
-              English
-            </Text>
-          </Box>
-          <BaseButton onClick={toggleMenu}>
-            <SideExpandIcon isOpen={open} />
-          </BaseButton>
-        </>
+        <Box display="flex" mb="22px" gridGap={[14]} alignItems="center">
+          <LanguageIcon />
+          <Text tag="span" variant="p3">
+            English
+          </Text>
+        </Box>
       )}
     </S.Wrapper>
   )
