177941d8dde31a13bd5dbd21df40ebb28d850ba3
Viachesalv Yahorlytskyi <peiko.yahorlytskyi.v@gmail.com>
2023-04-24 17:16:24 +0300

    I changed min length validation to 1 symbol for search input

--- src/features/search/SearchInput.tsx
+++ src/features/search/SearchInput.tsx
@@ -18,7 +18,7 @@ export const SearchInput: FC = () => {
     initialValues: {
       value: '',
     },
-    validationSchema: yup.object().shape({ value: validation.minWidth(2, t) }),
+    validationSchema: yup.object().shape({ value: validation.minWidth(1, t) }),
     onSubmit: ({ value }) => {
       if (value) router.push({ pathname: ROUTES.SEARCH, query: { value } })
     },
